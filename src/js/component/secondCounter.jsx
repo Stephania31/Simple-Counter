bb9a049ca5ba919e728fb87221f64ad0398cf206
Stephania31 <acunaantunez@gmail.com>
2023-02-25 23:01:18 +0000

    basicoTerminado

---
+++ src/js/component/secondCounter.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const SecondsCounter = (props) => {
+    return (
+      <>
+        <div className="container d-flex justify-content-center align-items-center mt-3 p-2 bg-dark">
+          <div className="row">
+          <div className="col-2 text-center me-2">
+              <h1>
+                <span className="badge fw-bold border border-dark-subtle bg-secondary-subtle">{<i class="fas fa-clock"></i>}</span>
+              </h1>
+            </div>
+          <div className="col-1 text-center me-3">
+              <h1>
+                <span className="badge fw-bold border border-light-subtle bg-secondary-subtle">{0}</span>
+              </h1>
+            </div>
+          <div className="col-1 text-center me-3">
+              <h1>
+                <span className="badge fw-bold border border-light-subtle bg-secondary-subtle">{0}</span>
+              </h1>
+            </div>
+          <div className="col-1 text-center me-3">
+              <h1>
+                <span className="badge fw-bold border border-light-subtle bg-secondary-subtle">{0}</span>
+              </h1>
+            </div>
+            <div className="col-1 text-center me-3">
+              <h1>
+                <span className="badge fw-bold border border-light-subtle bg-secondary-subtle">{props.centenaSeconds}</span>
+              </h1>
+            </div>
+            <div className="col-1 text-center me-3">
+              <h1>
+                <span className="badge fw-bold border border-light-subtle bg-secondary-subtle">{props.decenaSeconds}</span>
+              </h1>
+            </div>
+            <div className="col-1 text-center me-3">
+              <h1>
+                <span className="badge fw-bold border border-light-subtle bg-secondary-subtle">{props.seconds}</span>
+              </h1>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+export default SecondsCounter
--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,27 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import SecondsCounter from "./component/secondCounter.jsx";
 
+let tiempoSegundos = 0
+let decenaSeconds=0
+let centenaSeconds =0 
+
+setInterval(() => {
+    tiempoSegundos = tiempoSegundos + 1
+
+if(tiempoSegundos%10==0 && tiempoSegundos!=0){
+    tiempoSegundos=0
+    decenaSeconds++
+}
+if(decenaSeconds%6==0 && decenaSeconds!=0){
+    decenaSeconds=0
+    centenaSeconds++
+} 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SecondsCounter centenaSeconds={centenaSeconds} decenaSeconds={decenaSeconds} seconds={tiempoSegundos} />, document.querySelector("#app"));
+        
+}, 1000)
+
+
+
